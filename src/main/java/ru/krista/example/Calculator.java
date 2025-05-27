133a202cfd77cbc780e446ce1baec94bdeaa56c7
LavaOwl <verbdima@gmail.com@>
2025-05-27 11:18:37 +0300

    new comment

--- src/main/java/ru/krista/example/Calculator.java
+++ src/main/java/ru/krista/example/Calculator.java
@@ -1,4 +1,4 @@
-package ru.krista.example; //comment new
+package ru.krista.example; //comment
 
 import java.util.Scanner;
 
@@ -9,46 +9,42 @@ public class Calculator {
     public double num, num1, num2;
     public boolean isSign;
 
-    public Calculator(String line){
+    public Calculator(String line) {
         this.line = line;
         this.isSign = false;
     }
 
-    public void cut(){
+    public void cut() {
         this.slicedLine = this.line.split(" ");
-        
-        for(int i = 0; i <= 2; i++){
 
-            try{
+        for (int i = 0; i <= 2; i++) {
+
+            try {
                 this.num = Integer.parseInt(slicedLine[i]);
-            } catch(NumberFormatException e){
+            } catch (NumberFormatException e) {
                 this.isSign = true;
             }
 
-            if (this.isSign == true && i == 0 ){
+            if (this.isSign == true && i == 0) {
                 System.out.println("Ошибка: вместо первого числа введён сторонний символ!");
                 this.isSign = false;
                 break;
-            }
-            else if(this.isSign == false && i == 0){
+            } else if (this.isSign == false && i == 0) {
                 this.num1 = this.num;
             }
 
-            if (this.isSign == false && i == 1 ){
+            if (this.isSign == false && i == 1) {
                 System.out.println("Ошибка: вместо знака введён сторонний символ!");
                 break;
-            }
-            else if(this.isSign == true && i == 1){
+            } else if (this.isSign == true && i == 1) {
                 this.isSign = false;
             }
-            
 
-            if (this.isSign == true && i == 2 ){
+            if (this.isSign == true && i == 2) {
                 System.out.println("Ошибка: вместо второго числа введён сторонний символ!");
                 this.isSign = false;
                 break;
-            }
-            else if(this.isSign == false && i == 2){
+            } else if (this.isSign == false && i == 2) {
                 this.num2 = this.num;
             }
 
@@ -56,9 +52,8 @@ public class Calculator {
 
     }
 
-
-    public void calc(){
-        switch(this.slicedLine[1]) {
+    public void calc() {
+        switch (this.slicedLine[1]) {
             case "+":
                 plus();
                 break;
@@ -72,35 +67,33 @@ public class Calculator {
                 break;
 
             case "/":
-                if (this.num2 != 0){
+                if (this.num2 != 0) {
                     division();
                     break;
                 } else {
                     System.out.println("Ошибка: деление на 0!");
                     break;
                 }
-            
+
             default:
-            System.out.println("Ошибка: неправильный знак!");
+                System.out.println("Ошибка: неправильный знак!");
         }
     }
 
-    public void plus(){
+    public void plus() {
         System.out.println(this.num1 + this.num2);
     }
 
-    public void minus(){
+    public void minus() {
         System.out.println(this.num1 - this.num2);
     }
 
-    public void multyply(){
+    public void multyply() {
         System.out.println(this.num1 * this.num2);
     }
 
-    public void division(){
+    public void division() {
         System.out.println(this.num1 / this.num2);
     }
 
-    
-    
 }
